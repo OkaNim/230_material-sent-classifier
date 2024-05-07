708d1c41809022ce0074d9d9a58dbb6b3dbfcc08
OkaNim <OKA.Hiroyuki@nims.go.jp>
2024-05-07 10:01:44 +0900

    Add files via upload

--- src/230_test.py
+++ src/230_test.py
@@ -15,17 +15,15 @@ sents = [
 
 
 model_path_for_infer = "./230_file/ver=136_epoch=19_val_loss=0.873_SC_understand_220318"
-GPU_ID = "cpu"    # CPU:"cpu", GPU_0:[0], GPU_1:[1], , ,.
+device = "cpu"    # CPU:"cpu", GPU_0:[0], GPU_1:[1], , , .
 pret_model_path = # Write the path for the pre-trained model such as matscibert.
 
-results, Y_preds = src_186.infer(sents, model_path_for_infer, GPU_ID, pret_model_path, screen_print=True)
+results, Y_preds = src_186.infer(sents, model_path_for_infer, device, pret_model_path, screen_print=True)
 
 
 print("\n\nresults",)
-for x in results:
-    print("\n", x)
+for x in results: print("\n", x)
 
-
-print("\n\nY_preds\n\n", Y_preds, "\n\n")
-"""Y_preds are the label-ids for pytorch: e.g. title:0, aim:2, preparation:3."""
+print("\n\nY_preds\n\n", Y_preds)
+""" Y_preds are the label-ids: other:0, title:1, aim:2, preparation:3, , , . """
 
